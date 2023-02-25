6257ecb50b97243ed10376eed33ad0f5a47d39a2
pepe-popo <104313537+pepe-popo@users.noreply.github.com>
2023-02-25 17:18:17 +0300

    first task

---
+++ task_1/string_module.js
@@ -0,0 +1,59 @@
+/**
+ * gets a string and converts it to lowercase, but the first letter is uppercase
+ * 
+ * @param {string} str 
+ */
+export function myToLowerCase(str) {
+    return str[0]
+        .toUpperCase() + str.slice(1)
+            .toLowerCase();
+}
+
+/**
+ * gets a string and removes excess spaces
+ * 
+ * @param {string} str 
+ */
+export function fixSpaces(str) {
+    return str.split(' ') // пробелы конвертируются в пустые строки
+        .filter(char => char) // пустые строки съедаются фильтром
+        .join(' ')
+        .replace(' ,', ',')
+        .replace(' .', '.');
+}
+
+/**
+ * calculates the number of words in a string
+ * 
+ * @param {string} str 
+ * @returns {number} the number of words in a line
+ */
+export function howManyWords(str) {
+    return str.replaceAll(/[.,!?-]/g, '')
+        .split(' ')
+        .filter(char => char)
+        .length;
+}
+
+/**
+ * counts the number of unique words in a string and outputs a table of results
+ * 
+ * @param {string} str 
+ */
+export function uniqueWords(str) {
+    let result = { 'кол-во повторений': {} }
+
+    let arrayWords = str.replaceAll(/[.,!?-]/g, '')
+        .toLowerCase()
+        .split(' ');
+
+    for (let word of arrayWords) {
+        result['кол-во повторений'].hasOwnProperty(word) ?
+            result['кол-во повторений'][word]++
+            : result['кол-во повторений'][word] = 1;
+    }
+
+    console.table(result); // Вообще можно вернуть сам объект, а дальше уже как надо его обработать, но пусть будет так
+}
+
+uniqueWords("Текст, в котором слово текст несколько раз встречается и слово тоже");
---
+++ test/string_module.spec.js
@@ -0,0 +1,32 @@
+import { myToLowerCase, fixSpaces, howManyWords, uniqueWords } from "../task_1/string_module.js";
+import assert from "assert"
+
+describe("checking myToLowerCase", ()=>{
+    it('tEsTsTrInG to lower case', ()=> {
+        assert.equal(myToLowerCase("tEsTsTrInG"), "Teststring");
+    })
+})
+
+describe("checking fixSpaces", ()=>{
+    it('string correction', ()=> {
+        assert.equal(fixSpaces(` Пример  строки   , тут много    лишних               пробелов . `),
+         `Пример строки, тут много лишних пробелов.`);
+    })
+})
+
+describe("how many words", ()=>{
+    it('how many words', ()=> {
+        assert.equal(howManyWords("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt"),
+         13);
+    })
+
+    it('how many words 2', ()=> {
+        assert.equal(howManyWords("Текст, в котором слово текст несколько раз встречается и слово тоже"),
+         11);
+    })
+
+    it('how many words 3', ()=> {
+        assert.equal(howManyWords(" ,  !    . "),
+         0);
+    })
+})
